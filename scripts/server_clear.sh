c67c37479907e992e3f1879142ecf0932acc6b95
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-06-29 13:52:53 +0100

    1d

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-echo "hello"
-pkill -f "demo-0.0.1-SNAPSHOT.jar" || true
+isExistApp=$(pgrep java)
+if [[ -n  $isExistApp ]]; then
+  echo "Stopping the application."
+  sudo kill -9 $isExistApp
+  echo "The application is stopped."
+else
+  echo "The application is not running."
+fi
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar /home/ubuntu/server/demo-0.0.1-SNAPSHOT.jar
+nohup java -jar /home/ubuntu/server/demo-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
